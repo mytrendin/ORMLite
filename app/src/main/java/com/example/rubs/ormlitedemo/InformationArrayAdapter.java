5db7a38903513dda94bddedf7bb1b435903356d4
mytrendin <mytrendinn@gmail.com>
2017-05-18 15:52:09 +0530

    Initial Commit

--- app/src/main/java/com/example/rubs/ormlitedemo/InformationArrayAdapter.java
+++ app/src/main/java/com/example/rubs/ormlitedemo/InformationArrayAdapter.java
@@ -1,13 +1,10 @@
 package com.example.rubs.ormlitedemo;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.j256.ormlite.dao.Dao;
